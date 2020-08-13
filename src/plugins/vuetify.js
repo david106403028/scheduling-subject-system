f45a405f3e07fc2952a2a64dce262a713d4be7de
DavisLin <davislin@trevi.cc>
2020-08-13 23:05:11 +0800

    prettier

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,13 @@
-import Vue from "vue";
-import Vuetify from "vuetify/lib";
-import zhHant from "vuetify/es5/locale/zh-Hant";
+import '@mdi/font/css/materialdesignicons.css'
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import zhHant from 'vuetify/es5/locale/zh-Hant'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
-  lang: {
-    locales: { zhHant },
-    current: "zh-Hant"
-  }
-});
+    lang: {
+        locales: { zhHant },
+        current: 'zh-Hant'
+    }
+})
