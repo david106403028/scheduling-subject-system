f299ffeb4822da0442268efce16a502408d79864
DavisLin <davislin@trevi.cc>
2020-08-14 00:20:37 +0800

    default login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,35 @@
 <template>
-    <div>這是登入頁面</div>
+    <div class="login d-flex justify-center align-center">
+        <v-card color="red lighten-3">
+            <v-card-text class="pa-6">
+                <v-text-field class="mt-7" background-color="white" outlined hide-details></v-text-field>
+                <v-text-field class="mt-7" background-color="white" outlined hide-details></v-text-field>
+            </v-card-text>
+            <v-card-actions class="pa-6 d-flex">
+                <v-btn class="flex-grow-1" color="error" x-large :ripple="false">登入</v-btn>
+                <v-btn class="flex-grow-1" color="primary" x-large :ripple="false">註冊</v-btn>
+            </v-card-actions>
+        </v-card>
+    </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.login {
+    height: 100%;
+    width: 100%;
+}
+::v-deep .v-card {
+    height: 400px;
+    width: 800px;
+    &__text {
+        height: 70%;
+    }
+    &__actions {
+        height: 30%;
+    }
+}
+</style>
