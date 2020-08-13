e73b898443ca72b882cafb56b496a0518be5d996
DavisLin <davislin@trevi.cc>
2020-08-13 23:39:03 +0800

    wording

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
     <v-app>
         <v-app-bar app dark>
-            <router-link class="mr-4" to="/">Home</router-link>
-            <router-link class="mr-4" to="/audit">Audit</router-link>
-            <router-link class="mr-4" to="/login">login</router-link>
-            <router-link class="mr-4" to="/schedule">schedule</router-link>
-            <router-link class="mr-4" to="/subject">subject</router-link>
-            <router-link to="/register">register</router-link>
+            <router-link class="mr-4" to="/">主頁面</router-link>
+            <router-link class="mr-4" to="/audit">畢審</router-link>
+            <router-link class="mr-4" to="/login">登入</router-link>
+            <router-link class="mr-4" to="/schedule">排程</router-link>
+            <router-link class="mr-4" to="/subject">課程</router-link>
+            <router-link to="/register">註冊</router-link>
         </v-app-bar>
         <v-main>
             <v-container fluid fill-height pa-0>
