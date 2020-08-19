706f89e1736a535f4be39686161e38e1d829c2ae
DavisLin <davislin@trevi.cc>
2020-08-20 00:47:53 +0800

    update register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,9 +1,44 @@
 <template>
-    <div>這是註冊頁面</div>
+    <div class="register d-flex justify-center align-center">
+        <v-card color="red lighten-3">
+            <v-card-text class="pa-6">
+                <v-text-field class="mt-7" background-color="white" outlined hide-details label="Email"></v-text-field>
+                <v-text-field class="mt-7" background-color="white" outlined hide-details label="帳號"></v-text-field>
+                <v-text-field class="mt-7" background-color="white" outlined hide-details label="密碼"></v-text-field>
+                <v-text-field
+                    class="mt-7"
+                    background-color="white"
+                    outlined
+                    hide-details
+                    label="確認密碼"
+                ></v-text-field>
+            </v-card-text>
+            <v-card-actions class="pa-6 d-flex">
+                <v-btn class="flex-grow-1" color="primary" x-large :ripple="false" @click="$router.push('/login')">
+                    註冊
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.register {
+    height: 100%;
+    width: 100%;
+}
+::v-deep .v-card {
+    height: 500px;
+    width: 800px;
+    &__text {
+        height: 70%;
+    }
+    &__actions {
+        height: 30%;
+    }
+}
+</style>
