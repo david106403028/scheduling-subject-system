dd512fef00209fbe635a5a0ac9a4c03b20d4a187
DavisLin <davislin@trevi.cc>
2020-09-22 03:05:09 +0800

    draggable subject

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="schedule d-flex flex-row">
         <div class="left d-flex flex-column w-50 flex-grow-1 mr-4">
-            <table class="subject">
+            <table class="subject" @drop="drop($event)" @dragover="event => event.preventDefault()">
                 <thead>
                     <tr>
                         <th></th>
@@ -20,29 +20,59 @@
                         </template>
                         <template v-else>
                             <td class="sub" v-for="item in date" :key="item.subId" :id="item.subId">
-                                {{ item.subId }}
+                                <label>{{ item.subLable }}</label>
                             </td>
                         </template>
                     </tr>
                 </tbody>
             </table>
         </div>
-        <div class="right w-50 flex-grow-1"></div>
+        <div class="right w-50 flex-grow-1">
+            <div
+                class="subject-item draggable"
+                draggable="true"
+                v-for="item in availableSubject"
+                :key="item.subjectId"
+                @dragstart="drag(item)"
+            >
+                <div>{{ item.title }}</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    data: () => ({}),
+    data: () => ({
+        availableSubject: [
+            { title: '資尿褲', subjectId: 's0103034', classTime: '2020/09/21', score: 3 },
+            { title: '資當節', subjectId: 's0102035', classTime: '2020/09/21', score: 3 },
+            { title: '馬的法克', subjectId: 's0202036', classTime: '2020/09/21', score: 2 },
+            { title: '操你媽', subjectId: 's0109037', classTime: '2020/09/21', score: 1 },
+            { title: '我央', subjectId: 's0107038', classTime: '2020/09/21', score: 5 },
+            { title: '大頭包小頭', subjectId: 's0102039', classTime: '2020/09/21', score: 2 }
+        ],
+        dragTarget: null
+    }),
     computed: {
         scheduleCalanders() {
             let result = [1, 2, 3, 4, 9, 5, 6, 7, 8].map(value => {
-                let arr = new Array(6)
-                    .fill({ value })
-                    .map((cur, index) => ({ subId: `${cur.value}${!index ? '' : `-${index}`}` }))
+                let arr = new Array(6).fill({ value }).map((cur, index) => ({
+                    subId: `${cur.value}${!index ? '' : `-${index}`}`,
+                    subLable: !index ? cur.value : null
+                }))
                 return arr
             })
             return result
+        }
+    },
+    methods: {
+        drop(event) {
+            console.log(event)
+        },
+        drag(item) {
+            console.log(item)
+            this.dragTarget = item
         }
     }
 }
@@ -68,6 +98,24 @@ export default {
         border: 1px solid #c0c0c0;
         padding: 10px;
     }
+    &-item {
+        height: 40px;
+        padding: 5px 10px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        border: 1px solid #e0e1e2;
+        border-bottom: 0;
+        &:last-child {
+            border-bottom: 1px solid #e0e1e2;
+        }
+    }
+}
+.draggable {
+    user-select: none;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
 }
 .w-50 {
     width: 50%;
